8f96daa8ae4a4c4dbe37c16b7c6e5eb65f9f92d8
mcissel <mcissel@gmail.com>
2016-03-30 22:10:40 -0400

    adding google maps

--- client/app.ts
+++ client/app.ts
@@ -5,28 +5,50 @@ import {bootstrap, MeteorComponent} from 'angular2-meteor';
 import {AccountsUI} from 'meteor-accounts-ui';
 import {InjectUser} from 'meteor-accounts';
 
+import {ANGULAR2_GOOGLE_MAPS_PROVIDERS, ANGULAR2_GOOGLE_MAPS_DIRECTIVES, MapMouseEvent} from 'ng2-google-maps/core';
+
 import {Locations} from 'collections/locations';
 import {LocationsForm} from 'client/check-in-form/check-in-form';
 
 @Component({
-    selector: 'app'
+  selector: 'app'
 })
 @View({
-    templateUrl: 'client/app.html',
-    directives: [LocationsForm, ROUTER_DIRECTIVES, AccountsUI]
+  templateUrl: 'client/app.html',
+  directives: [LocationsForm, ROUTER_DIRECTIVES, ANGULAR2_GOOGLE_MAPS_DIRECTIVES, AccountsUI]
 })
 @InjectUser()
+
 class CheckIn extends MeteorComponent {
-    locations: Mongo.Cursor<Object>;
+  locations: Mongo.Cursor<Object>;
 
-    constructor () {
-      super();
-        this.locations = Locations.find();
-    }
+  // TYPES EXPECTED!!!
+  centerLat: Number = 39.058833;
+  centerLng: Number = -77.453420;
 
-    removeLocation(location) {
-        Locations.remove(location._id);
-    }
+  // Constructors
+  constructor () {
+    super();
+    this.locations = Locations.find();
+  }
+
+  removeLocation(location) {
+    Locations.remove(location._id);
+  }
+
+  // get? what is this, C#?
+  get lat(): Number {
+    return this.location && this.location.lat;
+  }
+
+  get lng(): Number {
+    return this.location && this.location.lng;
+  }
+
+  mapClicked($event: MapMouseEvent) {
+    this.location.lat = $event.coords.lat;
+    this.location.lng = $event.coords.lng;
+  }
 }
 
-bootstrap(CheckIn, [ROUTER_PROVIDERS]);
+bootstrap(CheckIn, [ROUTER_PROVIDERS, ANGULAR2_GOOGLE_MAPS_PROVIDERS]);
